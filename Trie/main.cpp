3feba83b01073a165b92c2defc0432ee20774617
Basti Hintermaier <hinterma@hm.edu>
2016-11-15 09:30:25 +0100

    Initial Commit

---
+++ Trie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Trie
+//
+//  Created by Basti Funtasty on 15/11/16.
+//  Copyright © 2016 Funtasty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
